26f7d68efe309fbce66c031e03046275e9b3c925
frinzekt <frinzekt@gmail.com>
2020-01-21 13:59:45 +0800

    InlineEditor and Base64 Custom Build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,10 +1,11 @@
 /**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
@@ -28,12 +29,14 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
-
+export class InlineEditor extends InlineEditorBase {}
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
+InlineEditor.builtinPlugins = ClassicEditor.builtinPlugins = [
 	Essentials,
+	Base64UploadAdapter,
 	UploadAdapter,
 	Autoformat,
 	Bold,
@@ -58,7 +61,7 @@ ClassicEditor.builtinPlugins = [
 ];
 
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+InlineEditor.builtinPlugins = ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
@@ -89,11 +92,7 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
+		contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
